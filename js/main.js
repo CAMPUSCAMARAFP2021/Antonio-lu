a4a8eec523422f334555e1c25a7f12d3d9189b03
luciaCalabrese <lcala419@gmail.com>
2021-12-01 09:41:41 +0100

    peticiones de los diferentes datos.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 linkgral = "https://api.themoviedb.org/3/"
 apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
+linkimg =`https://image.tmdb.org/t/p/w200/`
 
 const getPeliculasPopulares = () => {
     axios.get(`${linkgral}genre/movie/list${apiKey}`)
@@ -8,12 +9,14 @@ const getPeliculasPopulares = () => {
         const peliculas = res.data.genres;
         peliculas.forEach(generos => {
            
-      
     axios.get(`${linkgral}movie/popular${apiKey}`)
     .then(res =>{
         const peliculas = res.data.results;
         peliculas.forEach(pelicula => {
-            document.getElementById("tablaprueba").insertRow(-1).innerHTML = `<td></td><td>${pelicula.title}</td><td>${pelicula.overview}</td><td>${generos.name}</td>`;
+            if (pelicula.genre_ids[0] == generos.id) {
+                document.getElementById("tablaprueba").insertRow(-1).innerHTML = `<td><img src="${linkimg}${pelicula.backdrop_path}" alt=""></td><td>${pelicula.title}</td><td>${pelicula.overview}</td><td>${generos.name}</td>`;
+             }
+             
            
         })
     })
@@ -24,14 +27,7 @@ const getPeliculasPopulares = () => {
 
 
 const getPeliculas = () => {
-    axios.get(`${linkgral}genre/movie/list${apiKey}`)
-    .then(res =>{
-        const peliculas = res.data.genres;
-        peliculas.forEach(pelicula => {
-            document.getElementById("tablaprueba").insertRow(-1).innerHTML = `<td></td><td></td><td></td><td>${pelicula.name}</td>`;
-           
-        })
-    })
+  
 }
 getPeliculasPopulares()
 getPeliculas()
