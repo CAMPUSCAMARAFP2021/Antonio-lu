0ad3c8bb3a06bec84285a028021d1ccbb6f287c5
Antonio <acrocamora@gmail.com>
2021-12-01 12:15:27 +0100

    locura

--- dataprovider.js
+++
@@ -1,6 +0,0 @@
-class dataprovider {
-    linkgral = "https://api.themoviedb.org/3/"
-    apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
-
-
-}
---
+++ js/dataprovider.js
@@ -0,0 +1,82 @@
+class DataProvider {
+    
+    linkapi = "https://api.themoviedb.org/3/"
+    apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
+    linkimg =`https://image.tmdb.org/t/p/w200/`
+
+    endPoint(endPoint, params) {
+        const url = this.linkapi + endPoint + this.apiKey
+        return this.call(url, params)
+    }
+
+    call (url, params) {
+      return fetch(url)
+       .then(res => res.json())
+       .catch(this.errorHandle)
+    }
+
+    errorHandle(error){
+        throw new Error()
+    }
+
+
+}
+
+
+class MoviesDataProvider extends DataProvider {
+
+
+    endpoints = {
+        genres: '/genres/movie/list',
+        movies: '/movie',
+        populars: '/movie/populars'
+
+    }
+
+
+    getMovies(size = 10){
+        return this.endPoint(this.endpoints.movies,{size} )
+        .then(pelis => this.attachGenres(pelis.results))
+    }
+
+    attachGenres(pelis, genres){
+
+    }
+
+    getGenres(){
+        return this.endPoint(this.endpoints.genres)
+    }
+
+    getMovieData(idMovie) {
+
+    }
+
+}
+
+
+class MoviesFinder {
+
+    pelis
+
+    constructor() {
+        this.provider = new MoviesDataProvider();
+        this.loadData();
+    }
+    
+    async loadData() {
+       pelis = await this.provider.getMovies();
+       genres = await this.provider.getGenres();
+       this.pelis = this.provider.attachGenres(pelis, genres)
+    }
+
+    render() {
+        renderedPelis = this.pelis.map(this.renderPeli)
+    }
+
+    renderPeli(peli) {
+        const img = document.createElement('img').src = peli.poster_path;
+        img.alt = peli.title;
+        return document.createElement('div').append(img).innerHTML = peli.title;
+    }
+}
+
