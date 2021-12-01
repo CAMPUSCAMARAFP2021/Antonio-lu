1b34ad191b66ef01ca586c3d843be5aa990074cc
luciaCalabrese <lcala419@gmail.com>
2021-12-01 08:45:37 +0100

    cambio de rutas

--- appi2/appi.js
+++ appi2/appi.js
@@ -1,7 +1,7 @@
 
 
 const getPeliculasPopulares = () => {
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES')
+    axios.get(`${linkgral}movie/popular${apiKey}`)
     .then(res =>{
         const peliculas = res.data.results;// el body de la respuesta viene en la propiedad “data”
         peliculas.forEach(pelicula => {
@@ -11,14 +11,19 @@ const getPeliculasPopulares = () => {
     })
 }
 
+linkgral = "https://api.themoviedb.org/3/"
+apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
+
+
 
 const getPeliculas = () => {
     fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES')
     .then(res=>res.json())
     .then(res => {
-        const peliculas = res.results;
+        const peliculas = res.genres;
         peliculas.forEach(pelicula => {
             console.log(pelicula.name)
+            
         })
     })
 }
