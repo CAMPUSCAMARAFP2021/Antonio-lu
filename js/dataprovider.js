fc4b801cb7df40a4aaeee9387b3d947a505496e5
luciaCalabrese <lcala419@gmail.com>
2021-12-07 19:32:04 +0100

    Results of genres

--- js/dataprovider.js
+++ js/dataprovider.js
@@ -4,15 +4,20 @@ class DataProvider {
     apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
     linkimg =`https://image.tmdb.org/t/p/w200/`
 
+    link = "https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES"
+
     endPoint(endPoint, params) {
         const url = this.linkapi + endPoint + this.apiKey
         return this.call(url, params)
     }
 
-    call (url, params) {
-      return fetch(url)
-       .then(res => res.json())
-       .catch(this.errorHandle)
+    async call (url) {
+      try {
+            const res = await fetch(url)
+            return await res.json()
+        } catch (error) {
+            return this.errorHandle(error)
+        }
     }
 
     errorHandle(error){
@@ -26,27 +31,39 @@ class DataProvider {
 class MoviesDataProvider extends DataProvider {
 
     endpoints = {
-        genres: '/genres/movie/list',
-        movies: '/movie',
-        populars: '/movie/populars'
+        genres: '/genre/movie/list',
+        movies: 'movie',
+        populars: 'movie/popular'
     };
 
 
-    async getMovies(size = 10){
-        return pelis = await this.endPoint(this.endpoints.movies, { size })
+    async getMovies(){
+        return pelis = await this.endPoint(this.endpoints.populars)
+
     }
 
-    attachGenres(pelis, genres){
+     getpopMovies(){
+        return pelispop =  this.endPoint(this.endpoints.populars)
+    }
+
+    attachGenres(pelis){
         return pelis;
     }
 
-    getGenres(){
-        return this.endPoint(this.endpoints.genres)
-        
-    }
+    async getGenres(){
+        const res = await this.endPoint(this.endpoints.genres)
 
-
+        //Nos devuelve los resultados de la promesa
+        .then(ress =>{
+            const peliculas = ress.genres;
+            peliculas.forEach(pelicula => {
+                console.log(pelicula.name)
+            })
+        })
 }
+}
+ejemplo = new MoviesDataProvider
+
 
 
 class MoviesFinder {
@@ -75,5 +92,4 @@ class MoviesFinder {
     }
 }
 
-movie = new MoviesFinder.render()
-console.log(movie)
+
