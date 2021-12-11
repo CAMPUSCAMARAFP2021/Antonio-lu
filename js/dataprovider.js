719bc6c95670f96ee3fc55b00d0e986ab47a1321
luciaCalabrese <lcala419@gmail.com>
2021-12-11 15:17:00 +0100

    Print names of movies

--- js/dataprovider.js
+++ js/dataprovider.js
@@ -2,13 +2,18 @@ class DataProvider {
 
     linkapi = "https://api.themoviedb.org/3/"
     apiKey = `?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`
-    linkimg = `https://image.tmdb.org/t/p/w200/`
+    linkimg = `https://image.tmdb.org/t/p/w200`
 
 
 
     endPoint(endPoint, params) {
         const url = this.linkapi + endPoint + this.apiKey
         return this.call(url, params)
+    }
+
+    linkimgs(endPoint) {
+        const url = this.linkimg + endPoint 
+        return this.call(url)
     }
 
     async call(url) {
@@ -27,6 +32,7 @@ class DataProvider {
 
 }
 
+hola = new DataProvider
 
 class MoviesDataProvider extends DataProvider {
 
@@ -37,9 +43,15 @@ class MoviesDataProvider extends DataProvider {
     };
 
 
-    async getMovies() {
+    getMovies() {
         const pelis = this.endPoint(this.endpoints.populars)
         return pelis
+    }
+
+    async getimg() {
+        const pelisTitle = await this.getMoviesWithGenres().then((respuesta) => respuesta.map(respuesta => respuesta.backdrop_path))
+        const img = this.endPoint(pelisTitle)
+        return img
     }
 
 
@@ -56,6 +68,8 @@ class MoviesDataProvider extends DataProvider {
         return this.getMovies().then(({ results }) => results.map(movie => this.attachGenresToMovie(movie, genres)))
     }
 
+
+
     getgenreMoviesname = async () => {
         const pelis = await this.getMoviesWithGenres()
         pelis.forEach(element => {
@@ -69,34 +83,54 @@ class MoviesDataProvider extends DataProvider {
     }
 
     getMoviestitle = async () => {
-       const pelisTitle = await this.getMoviesWithGenres().then((respuesta) => respuesta.map(respuesta => respuesta.original_title))
+        const pelisTitle = await this.getMoviesWithGenres().then((respuesta) => respuesta.map(respuesta => respuesta.original_title))
         return pelisTitle
     }
 
-    getSrcPelis = async() => {
+    getSrcPelis = async () => {
         const pelisTitle = await this.getMoviesWithGenres().then((respuesta) => respuesta.map(respuesta => respuesta.backdrop_path))
         return pelisTitle
     }
 
+    async getimg (){
+        const img = this.getSrcPelis().then(respuesta => this.linkimgs(respuesta).then(resp => resp))
+        return img
+        }
 
     // este metodo cambia el array de ids de generos de un "movie" por un array de generos ( objeto )
     attachGenresToMovie = (movie, genres) => {
         movie.genre_ids = movie.genre_ids.map((genreId => genres.find(genre => genre.id === genreId)));
         return movie;
     }
+    imprimirtitulo() {
+        const imagen = document.createElement('p')
+        const tit = this.getMoviestitle().then(resp => {
+            imagen.textContent =resp
+            resp.map((resp) =>
+                document.getElementById("tablaprueba").insertRow(-1).innerHTML = `<td>${resp}</td>`
+            )
+            
+            document.body.appendChild(imagen);
+        }
+            )
+        
+    }
 }
 
 ejemplo = new MoviesDataProvider
 
-class imprimir {
+class imprimir extends MoviesDataProvider {
 
-    imprimirImagenes () {
-        imagen = document.createElement('p')
-        
+    imprimirImagenes() {
+
+        const imagen = document.createElement('img')
+        imagen.src = MoviesDataProvider.getSrcPelis
+        document.body.appendChild(imagen);
     }
 
 }
 
+hola = new imprimir
 
 class MoviesFinder {
 
